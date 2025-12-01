7ef2c5a484410ef36a2b5c90061d2631b9e4061a
William Dahlberg <dahlberg.william@gmail.com>
2025-12-01 11:21:59 +0100

    dial at 50

--- 1.py
+++ 1.py
@@ -1,11 +1,11 @@
 
-dial = p1 = p2 = 0
+dial, p1, p2 = 50, 0, 0
 for line in open('1.input'):
     for _ in range(int(line[1:])): 
         dial = dial + 1 if line[0] == "R" else dial - 1
-        if (dial + 50) % 100 == 0:
+        if dial % 100 == 0:
             p2 += 1
-    if (dial + 50) % 100 == 0:
+    if dial % 100 == 0:
         p1 += 1
 
 print(p1, p2)
