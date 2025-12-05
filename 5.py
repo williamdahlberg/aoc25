ea7f5664c918d34f03413e45522b55be0bfca408
williamdahlberg <58395059+williamdahlberg@users.noreply.github.com>
2025-12-05 19:36:09 +0100

    remove p2 first solution

--- 5.py
+++ 5.py
@@ -5,22 +5,6 @@ ranges = sorted([tuple(map(int, r.split("-"))) for r in ranges])
 print(sum([any([a <= int(ingredient) <= b for a, b in ranges]) for ingredient in ingredients]))
 
 # p2
-merged = set()
-new_ranges = []
-for i, (a, b) in enumerate(ranges):
-    if i in merged:
-        continue
-    merged.add(i)
-
-    for j, (x, y) in enumerate(ranges[i:]):
-        if x <= b:
-            merged.add(i + j)
-            b = max(b, y)
-
-    new_ranges.append((a, b))
-print(sum([b-a+1 for a, b in new_ranges]))
-
-# p2 again
 total = low = 0
 for a, b in sorted(ranges):
     low = max(a, low + 1)
