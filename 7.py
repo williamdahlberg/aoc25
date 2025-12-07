da7c0131549e60da9bfd05a5f33b904c2ad901be
William Dahlberg <dahlberg.william@gmail.com>
2025-12-07 13:16:19 +0100

    no need for set

--- 7.py
+++ 7.py
@@ -3,14 +3,14 @@ lines = [l.strip() for l in open("7.input")]
 answer = [[1 if char == "S" else 0 for char in lines[0]]]
 for _ in range(len(lines[0])): answer.append([0] * len(lines[0]))
 
-hit_split = set()
+splits = 0
 for y, line in enumerate(lines[1:], 1):
     for x, char in enumerate(line):
         if char == "^":
-            if answer[y-1][x] > 0: hit_split.add((x,y))
+            if answer[y-1][x] > 0: splits += 1
             answer[y][x + 1] += answer[y-1][x]
             answer[y][x - 1] += answer[y-1][x]
         else:
             answer[y][x] += answer[y-1][x]
 
-print(len(hit_split), sum(answer[-1]))
+print(splits, sum(answer[-1]))
