d5030d36e3d56deef9d61e3e8834f34bb63aca53
williamdahlberg <58395059+williamdahlberg@users.noreply.github.com>
2025-12-08 12:16:53 +0100

    undo

--- 8.py
+++ 8.py
@@ -5,10 +5,14 @@ from functools import reduce
 
 points = [tuple(map(int, coords.split(","))) for coords in open("8.input")]  # from input
 distances = sorted([(pair, dist(*pair)) for pair in combinations(points, r=2)], key=itemgetter(1))
-circuits = {{p} for p in points}
+circuits = [{p} for p in points]
 for (a, b), _ in distances[:1000]:
-    matching_circuits = set(filter(lambda c: {a, b} - c and (a in c or b in c), circuits))
-    circuits -= matching_circuits
-    circuits.add(set.union(*matching_circuits))
+    new_set = set()
+    for c in circuits.copy():
+        if {a, b} - c and (a in c or b in c):
+            new_set |= c
+            circuits.remove(c)
+    if new_set:
+        circuits.append(new_set)
 
 print(reduce(mul, [len(c) for c in sorted(circuits, key=len, reverse=True)[:3]]))
