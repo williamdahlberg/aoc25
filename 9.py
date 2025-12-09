e3cb376b56c06ac4568aa5268c7b8ba6a458fe19
William Dahlberg <dahlberg.william@gmail.com>
2025-12-09 23:36:10 +0100

    part 2...

--- 9.py
+++ 9.py
@@ -1,3 +1,17 @@
+# p1
 from itertools import combinations
-def size(p1, p2): return (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
+def size(a, b): return (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1)
 print(max([size(*p) for p in combinations(list(map(eval, open("9.input"))), 2)]))
+
+# p2
+from shapely import LineString, MultiPoint, Point, Polygon
+
+reds = list(map(eval, open("9.input")))
+poly = Polygon(reds)
+squares = list(combinations(reds, 2))
+
+def square_polys(a, b):
+    c, d = (a[0], b[1]), (b[0], a[1])
+    return {LineString(line) for line in ((a,c), (c,b), (b,d), (d,a))}
+
+print(max([size(*square) for square in squares if all([poly.covers(point) for point in square_polys(*square)])]))
