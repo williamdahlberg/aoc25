c0fa89c329d0a78937a9d34354bf708bc2cf1a71
williamdahlberg <58395059+williamdahlberg@users.noreply.github.com>
2025-12-09 09:18:01 +0100

    p1 short

--- 9.py
+++ 9.py
@@ -1,8 +1,3 @@
 from itertools import combinations
-points = list(map(eval, open("9.input")))
-
-def size(p1, p2):
-    return (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
-
-sizes = sorted([size(*pair) for pair in combinations(points, r=2)])
-print(sizes[-1])
+def size(p1, p2): return (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
+print(max([size(*p) for p in combinations(list(map(eval, open("9.input"))), 2)]))
