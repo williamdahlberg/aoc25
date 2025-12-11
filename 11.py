87fa2416708f0ef3580d82ff126d2681136e3aa3
williamdahlberg <58395059+williamdahlberg@users.noreply.github.com>
2025-12-11 12:21:35 +0100

    p2

--- 11.py
+++ 11.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from functools import cache
 
 devices = {this[:-1]: outputs for this, *outputs in [line.split() for line in open("11.input")]}
 G = nx.DiGraph(devices)
@@ -13,3 +14,13 @@ def count(this, curr=1):
     return sum([count(that) for that in devices[this]])
 
 print(count("you"))
+
+# p2
+@cache
+def count(this, fft=False, dac=False):
+    if this == "out": return int(fft and dac)
+    if this == "fft": fft = True
+    if this == "dac": dac = True
+    return sum([count(that, fft, dac) for that in devices[this]])
+
+print(count("svr"))
